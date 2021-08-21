d7f4b33b789c91c5aa433ba819a4c4034a123804
eldadmwangi <eldadmwangi@gmail.com>
2021-08-21 15:30:49 +0300

    added firebase database to index.htm and firebase.js

---
+++ firebase.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBRti9kBwEqlikQoCzGnXHT8MQLASOMbCw",
+    authDomain: "vanilla-be4bb.firebaseapp.com",
+    projectId: "vanilla-be4bb",
+    storageBucket: "vanilla-be4bb.appspot.com",
+    messagingSenderId: "894233959164",
+    appId: "1:894233959164:web:a1d03adc487d800cfc5579",
+    measurementId: "G-PP6T6TNPSP"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
